60200544481f527d3a0f3a6b4cf159e422115b8c
puraanraai <puraanraai77@gmail.com>
2025-01-31 19:03:25 +0545

    User authentication management with roles

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,15 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'rolemanager:customer'])->name('dashboard');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin');
+})->middleware(['auth', 'verified', 'rolemanager:admin'])->name('admin');
+
+Route::get('/artist/dashboard', function () {
+    return view('artist');
+})->middleware(['auth', 'verified', 'rolemanager:artist'])->name('artist');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
